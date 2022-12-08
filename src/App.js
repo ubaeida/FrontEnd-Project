1b6a67513ae050afb6fe22f86a69b0a4f837486a
ubaeida <ubaeida.alkayal@gmail.com>
2022-12-08 14:31:11 +0300

    Rigstration componetnt is ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Register from "./pages/register/Register";
+
+const Register = React.lazy(() => import("./pages/register/Register"));
+const Login = React.lazy(() => import("./pages/Login/Login"));
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Suspense> <Register /></Suspense>}/>
+        <Route path="/register" element={<Suspense> <Register /></Suspense>}/>
+        <Route path="/login" element={<Suspense><Login /></Suspense>}/>
       </Routes>
     </>
   );
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,14 +1,94 @@
-import classes from './Register.css'
-const Register = () => { 
-    return( 
-    <div className='container'>
-        <div className='row'>
-            <div className='col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3'>
-                
-            </div> 
-        </div>
-    </div>
-    )
-}
+import classes from "./Register.module.css";
+import { Link , useNavigate} from "react-router-dom";
 
-export default Register
+const Register = () => {
+  const navigate = useNavigate()
+  const register = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${process.env.REACT_APP_API_REGISTER}`, {
+      method: "POST",
+      body: new FormData(e.target),
+    });
+    const json = await response.json();
+    console.log('response', json)
+    if (json.success) {
+      navigate('/login')
+    } else alert(json.messages);
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div
+          className={`col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3`}
+        >
+          <div className={`${classes.registerContiner} p-5 my-5`}>
+            <div className={`${classes.img} mb-4`}>
+              <img src="logo.svg" alt="logo" />
+            </div>
+            <h1 className="mb-4">Create Account</h1>
+            <form onSubmit={register} method="POST">
+              <div className="form-field mb-3">
+                <label htmlFor="name" className="mb-2">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-field mb-3">
+                <label htmlFor="email" className="mb-2">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-field mb-3">
+                <label htmlFor="password" className="mb-2">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-field mb-3">
+                <label htmlFor="passwordConfirmation" className="mb-2">
+                  Password Conformation
+                </label>
+                <input
+                  type="password"
+                  name="password_confirmation"
+                  id="passwordConfirmation"
+                  className="form-control"
+                />
+              </div>
+              <div className="row mt-5 align-items-center">
+                <div className="col-5">
+                  <Link to={`/login`}>Login</Link>
+                </div>
+                <div className="col-7">
+                  <input
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    value="Register"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
